35beb8d0f80f70593bd07e9b4512fb038a3181e4
nghlong3004 <nghlong3004@gmail.com>
2025-11-28 22:54:54 +0700

    chore: creational design patterns

---
+++ daily/2025/11/2025-11-28/review/creational-design-patterns/src/main/java/com/vn/nghlong3004/abstract_factory/DarkTextbox.java
@@ -0,0 +1,13 @@
+package com.vn.nghlong3004.abstract_factory;
+
+/**
+ * Project: creational-design-patterns
+ *
+ * @author nghlong3004
+ * @since 11/28/2025
+ */
+public class DarkTextbox implements Textbox {
+  public void render() {
+    System.out.println("Dark Textbox");
+  }
+}
---
+++ daily/2025/11/2025-11-28/review/creational-design-patterns/src/main/java/com/vn/nghlong3004/factory_method/NotificationFactory.java
@@ -0,0 +1,11 @@
+package com.vn.nghlong3004.factory_method;
+
+/**
+ * Project: creational-design-patterns
+ *
+ * @author nghlong3004
+ * @since 11/28/2025
+ */
+public interface NotificationFactory {
+  Notification create();
+}
